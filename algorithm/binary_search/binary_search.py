fbbb6ba7ba17aafc30308cdd555c723a2f25fc22
EvanA <amedievan@icloud.com>
2024-02-01 20:14:19 -0500

    recursive & more pythonic

--- algorithm/binary_search/binary_search.py
+++ algorithm/binary_search/binary_search.py
@@ -1,22 +1,22 @@
-
-def binary_search(array_list: list, number: int):
-	
-	low = 0
-	high = len(array_list) - 1
-	
-	while low <= high:
-		middle_index = (high + low) // 2
+def binary_search(array_list: list, low: int, high: int, number: int):
+	if high >= low:
+		mid = (high + low) // 2
 		
-		if array_list[middle_index] == number:
-			return middle_index
+		# if the element is present at the middle i"#945200"sself
+		if array_list[mid] == number:
+			return mid
 			
-		if array_list[middle_index] > number:
-			high = middle_index - 1
+		# if the element is smaller than mid, then go left
+		elif array_list[mid] > number:
+			return binary_search(array_list, low, mid - 1, number)
 			
+		# else the element can only be present in the right subarray
 		else:
-			low = middle_index - 1
-	
-	return -1
+			return binary_search(array_list, mid + 1, high, number)
+			
+	else:
+		# integer not present in array
+		return -1
 	
 	
 		
@@ -28,7 +28,7 @@ def main():
 	
 	array = [2, 3, 4, 7, 10, 13, 17, 21, 24, 29, 35, 37, 40, 57]
 	x = 29
-	result = binary_search(array, x)
+	result = binary_search(array, 0, len(array) - 1, x)
 	
 	if result == -1:
 		print(f"Integer is not present in the array")
@@ -38,3 +38,29 @@ def main():
 
 if __name__ == "__main__":
 	main()
+	
+
+
+
+# old code below:
+#
+#
+
+# def binary_search(array_list: list, number: int):
+	
+#	low = 0
+#	high = len(array_list) - 1
+	
+#	while low <= high:
+#		middle_index = (high + low) // 2
+		
+#		if array_list[middle_index] == number:
+#			return middle_index
+			
+#		if array_list[middle_index] > number:
+#			high = middle_index - 1
+			
+#		else:
+#			low = middle_index - 1
+	
+#	return -1
